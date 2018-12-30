b44716ebfee5b51f377e809bd81ceaccb3ab0561
Bartosz Taczała <bartosz.taczala@mobica.com>
2018-12-30 21:29:52 +0100

    Using fmt for string formatting

--- core/themoviedb/tmdscrapper.cpp
+++ core/themoviedb/tmdscrapper.cpp
@@ -2,16 +2,32 @@
 #include <curlpp/Easy.hpp>
 #include <curlpp/Options.hpp>
 
+#include <fmt/format.h>
+
+#include "config.h"
+
+namespace {
+const std::string kApiUrl = "https://api.themoviedb.org";
+const std::string kApiVersion = "3";
+}  // namespace
+
 namespace kfl {
 namespace themoviedb {
 Scrapper::Scrapper() { curlpp::initialize(); }
 Scrapper::~Scrapper() { curlpp::terminate(); }
 void Scrapper::findTitle(const std::string& title) {
     curlpp::Easy request;
-    request.setOpt(curlpp::Options::Url(
-        "https://api.themoviedb.org/3/search/"
-        "movie?api_key=749f20c38150f3dd95371aded810068b&language=en-US&query="
-        "holidays&page=1&include_adult=false"));
+    const std::string url = fmt::format(
+        "{url}/{apiVersion}/{command}/"
+        "movie?api_key={apiKey}&language={language}&query={query}&page=1&"
+        "include_adult=false",
+        fmt::arg("url", kApiUrl), fmt::arg("apiVersion", kApiVersion),
+        fmt::arg("apiKey", kfl::themoviedb::getApiKey()),
+        fmt::arg("query", title), fmt::arg("language", "en-US"),
+        fmt::arg("command", "search"));
+
+    std::cout << url << "\n";
+    request.setOpt(curlpp::Options::Url(url));
 
     request.perform();
     std::cout << request << "\n";
